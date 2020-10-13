548ffe6c923e505829bcef6215a2b07b058e58e6
黄可健 <huangkejian@acad.com.cn>
2020-10-13 14:16:40 +0800

    修改单元测试chai+mocha

---
+++ 1.2.7 单元测试/src/components/counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h3>counter.vue</h3>
+      <span>{{count}}</span>
+      <button @click="increase">自增</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            count:0
+        }
+    },
+    methods:{
+        increase(){
+            this.count++
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
