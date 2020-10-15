38f31ea149bdd64854c7d6d69307708155c4aaf9
黄可健 <huangkejian@acad.com.cn>
2020-10-15 18:17:27 +0800

    修改测试架构

---
+++ 1.2.7 单元测试/src/components/AsyncAxios.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="fetchResults">{{ value }}</button>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        value:'点击'
+      }
+    },
+
+    methods: {
+      async fetchResults() {
+        const response = await axios.post('https://jy.xixi.top/schoolservice/api/v1/schoolAdmin/common/listRegion')
+        console.log("_______+++++++++++",response.data.errcode)
+        this.value = response.data.errcode
+      }
+    }
+  }
+</script>
--- 1.2.7 单元测试/tests/unit/counter.spec.js
+++ 1.2.7 单元测试/tests/unit/counter.spec.js
@@ -4,7 +4,7 @@ import CounterChild from '@/components/CounterChild.vue'
 import { expect } from 'chai';
 import { mount } from '@vue/test-utils'
 
-describe('测试Counter.vue', () => {
+describe('网易云例子', () => {
     it('点击按钮后，count的值会+1处理', () => {
         // 获取DOM
         const Constructor = Vue.extend(Counter);   // 实例
@@ -18,10 +18,7 @@ describe('测试Counter.vue', () => {
         // 断言
         expect(Number(vm.$el.querySelector('span').textContent)).to.equal(1)
     })
-})
 
-
-describe('测试vue-test-unit的用例', () => {
     it('获取元素并断言值是否一致', () => {
         // 获取元素
         const wrapper = mount(Counter)
@@ -30,68 +27,68 @@ describe('测试vue-test-unit的用例', () => {
     })
 })
 
-describe('Vue Test Utils官方例子', () => {
+// describe('Vue Test Utils官方例子', () => {
 
-    // 现在挂载组件，你便得到了这个包裹器
-    const wrapper = mount(Counter, {
-        // 仿造Prop
-        propsData: {
-            foo: 'some value'
-        },
-        mocks: {
-            // 在挂载组件之前 添加仿造的 `$route` 对象到 Vue 实例中
-            $route: {
-                path: '/',
-                hash: '',
-                params: { id: '123' },
-                query: { q: 'hello' }
-            }
-        }
-    })
+//     // 现在挂载组件，你便得到了这个包裹器
+//     const wrapper = mount(Counter, {
+//         // 仿造Prop
+//         propsData: {
+//             foo: 'some value'
+//         },
+//         mocks: {
+//             // 在挂载组件之前 添加仿造的 `$route` 对象到 Vue 实例中
+//             $route: {
+//                 path: '/',
+//                 hash: '',
+//                 params: { id: '123' },
+//                 query: { q: 'hello' }
+//             }
+//         }
+//     })
 
-    it('挂载组件', () => {
-        // 获取Vue 实例
-        const vm = wrapper.vm
-    })
+//     it('挂载组件', () => {
+//         // 获取Vue 实例
+//         const vm = wrapper.vm
+//     })
 
-    it('测试组件渲染出来的 HTML', () => {
-        expect(wrapper.html()).contains('<span>0</span>')
-    })
+//     it('测试组件渲染出来的 HTML', () => {
+//         expect(wrapper.html()).contains('<span>0</span>')
+//     })
 
-    it('检查已存在的元素', () => {
-        const button = wrapper.find('button')
-        expect(button.exists()).to.be.true
-    })
+//     it('检查已存在的元素', () => {
+//         const button = wrapper.find('button')
+//         expect(button.exists()).to.be.true
+//     })
 
-    it('模拟用户交互', () => {
-        expect(wrapper.vm.count).to.equal(0)
-        const button = wrapper.find('button')
-        button.trigger('click')
-        expect(wrapper.vm.count).to.be.equal(1)
-    })
+//     it('模拟用户交互', () => {
+//         expect(wrapper.vm.count).to.equal(0)
+//         const button = wrapper.find('button')
+//         button.trigger('click')
+//         expect(wrapper.vm.count).to.be.equal(1)
+//     })
 
-    it('使用 nextTick 与 await', async () => {
-        expect(wrapper.vm.count).to.equal(1)
-        const button = wrapper.find('button')
-        await button.trigger('click')
-        expect(wrapper.vm.count).to.be.equal(2)
-    })
+//     it('使用 nextTick 与 await', async () => {
+//         expect(wrapper.vm.count).to.equal(1)
+//         const button = wrapper.find('button')
+//         await button.trigger('click')
+//         expect(wrapper.vm.count).to.be.equal(2)
+//     })
 
-    it('从子组件触发事件', async () => {
-        await wrapper.find(CounterChild).vm.$emit('custom')
-        expect(wrapper.html()).contains('Emitted!')
-    })
+//     it('从子组件触发事件', async () => {
+//         await wrapper.find(CounterChild).vm.$emit('custom')
+//         expect(wrapper.html()).contains('Emitted!')
+//     })
 
-    it('仿造Prop', () => {
-        expect(wrapper.vm.foo).to.be.equal('some value')
-    })
+//     it('仿造Prop', () => {
+//         expect(wrapper.vm.foo).to.be.equal('some value')
+//     })
 
-    it('操作组件状态', async () => {
-        await wrapper.setData({ count: 10 })
-        expect(wrapper.vm.count).to.be.equal(10)
+//     it('操作组件状态', async () => {
+//         await wrapper.setData({ count: 10 })
+//         expect(wrapper.vm.count).to.be.equal(10)
 
-        await wrapper.setProps({ foo: 'bar' })
-        expect(wrapper.vm.foo).to.be.equal('bar')
-    })
+//         await wrapper.setProps({ foo: 'bar' })
+//         expect(wrapper.vm.foo).to.be.equal('bar')
+//     })
 
-})
+// })
